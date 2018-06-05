74f01f9d20618ba848353f3c64d280ed70f553b3
qianh <qianh@bitech.cn>
2018-06-05 09:56:12 +0800

    use https protocol

--- ChineseHoliday.cs
+++ ChineseHoliday.cs
@@ -4,58 +4,68 @@ using System.Net;
 
 public class ChineseHoliday
 {
-    static string specificHolidayUrl = "http://gameboylv.github.io/ChineseHoliday/data/{0}.txt";
-    static string specificWorkdayUrl = "http://gameboylv.github.io/ChineseHoliday/data/{0}_w.txt";
-    static TimeSpan workStartTime = new TimeSpan(9, 0, 0);
-    static TimeSpan workEndTime = new TimeSpan(17, 30, 0);
+	static string specificHolidayUrl = "https://gameboylv.github.io/ChineseHoliday/data/{0}.txt";
+	static string specificWorkdayUrl = "https://gameboylv.github.io/ChineseHoliday/data/{0}_w.txt";
+	static TimeSpan workStartTime = new TimeSpan(9, 0, 0);
+	static TimeSpan workEndTime = new TimeSpan(17, 30, 0);
 
-    static List<DateTime> holidayOfYear(int year)
-    {
-        var holiday = new List<DateTime>();
-        for (var i = new DateTime(year, 1, 1); i < new DateTime(year + 1, 1, 1); i = i.AddDays(1))
-        {
-            if (i.DayOfWeek == DayOfWeek.Saturday || i.DayOfWeek == DayOfWeek.Sunday)
-                holiday.Add(i);
-        }
+	static List<DateTime> holidayOfYear(int year)
+	{
+		var holiday = new List<DateTime>();
+		for (var i = new DateTime(year, 1, 1); i < new DateTime(year + 1, 1, 1); i = i.AddDays(1))
+		{
+			if (i.DayOfWeek == DayOfWeek.Saturday || i.DayOfWeek == DayOfWeek.Sunday)
+				holiday.Add(i);
+		}
 
-        try
-        {
-            var specificHolidays = new WebClient().DownloadString(string.Format(specificHolidayUrl, year));
-            foreach (var i in specificHolidays.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries))
-            {
-                var day = DateTime.Parse(year + "-" + i.Substring(0, 2) + "-" + i.Substring(2, 2));
-                if (!holiday.Contains(day))
-                    holiday.Add(day);
-            }
+		try
+		{
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+			var specificHolidays = new WebClient().DownloadString(string.Format(specificHolidayUrl, year));
+			foreach (var i in specificHolidays.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries))
+			{
+				var day = DateTime.Parse(year + "-" + i.Substring(0, 2) + "-" + i.Substring(2, 2));
+				if (!holiday.Contains(day))
+					holiday.Add(day);
+			}
 
-            var specificWorkday = new WebClient().DownloadString(string.Format(specificWorkdayUrl, year));
-            foreach (var i in specificWorkday.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries))
-            {
-                var day = DateTime.Parse(year + "-" + i.Substring(0, 2) + "-" + i.Substring(2, 2));
-                if (holiday.Contains(day))
-                    holiday.Remove(day);
-            }
-        }
-        catch
-        {
+			var specificWorkday = new WebClient().DownloadString(string.Format(specificWorkdayUrl, year));
+			foreach (var i in specificWorkday.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries))
+			{
+				var day = DateTime.Parse(year + "-" + i.Substring(0, 2) + "-" + i.Substring(2, 2));
+				if (holiday.Contains(day))
+					holiday.Remove(day);
+			}
+		}
+		catch
+		{
 
-        }
-        holiday.Sort();
-        return holiday;
-    }
+		}
+		holiday.Sort();
+		return holiday;
+	}
 
-    static Dictionary<int, List<DateTime>> holiday = new Dictionary<int, List<DateTime>>();
+	static Dictionary<int, List<DateTime>> holiday = new Dictionary<int, List<DateTime>>();
 
-    public static List<DateTime> GetHoliday(int year)
-    {
-        if (!holiday.ContainsKey(year))
-            holiday[year] = holidayOfYear(year);
-        return holiday[year];
-    }
+	public static List<DateTime> GetHoliday(int year)
+	{
+		if (!holiday.ContainsKey(year))
+			holiday[year] = holidayOfYear(year);
+		return holiday[year];
+	}
 
-    public static bool IsWorkingTime(DateTime time)
-    {
-        return !GetHoliday(time.Year).Contains(time.Date) && time.TimeOfDay >= workStartTime && time.TimeOfDay < workEndTime;
-    }
+	public static List<DateTime> GetHoliday(int year, int month)
+	{
+		return GetHoliday(year).FindAll(item => item.Month == month);
+	}
 
+	public static bool IsWorkingDay(DateTime time)
+	{
+		return !GetHoliday(time.Year).Contains(time.Date);
+	}
+
+	public static bool IsWorkingTime(DateTime time)
+	{
+		return IsWorkingDay(time) && time.TimeOfDay >= workStartTime && time.TimeOfDay < workEndTime;
+	}
 }
